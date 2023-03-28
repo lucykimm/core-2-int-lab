251c1dec77811aff1a687e858f4a088fd205cde3
lucykimm <kimy528@newschool.edu>
2023-03-28 16:19:38 -0400

    0328-1

---
+++ activities/display-data-v2/assets/script.js
@@ -0,0 +1,57 @@
+
+let letters = ['a', 'b', 'c']; //an array;
+
+let emojiObject = {
+    "key": "value",
+    "id": 13,
+    "emoji": "👍"
+};
+
+//setup container element
+let container = document.getElementById("container");
+
+
+fetch('./assets/emojis-v3.json')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    processEmojis(data);
+  })
+  .catch(error => console.log(error));
+
+function processEmojis( data ){
+  data.forEach( function(item, index){
+    console.log(item, index);
+    let usage = item['general-usage'] * 10;
+    console.log('usage', usage);
+    let newItem = document.createElement("div");
+    newItem.classList.add("icon");
+    newItem.classList.add(item.categorykey);
+    // newItem.style.cssText = `font-size: ${usage}px`;
+    //if setting up new pages /links
+    // newItem.innerHTML = `
+    // <a href="${item.linkname}.html"><div class="category">${item.category}</div>
+    // <div class="emoji">${item.emoji}</div>
+    // <div class="detail">
+    //     ${item.emoji}<br>
+    //     <p class="description">${item.description}</div>
+    // </div></a>
+    // `;
+
+    newItem.innerHTML = `
+    <div class="category">${item.category}</div>
+    <div class="emoji">${item.emoji}</div>
+    <div class="detail">
+        ${item.emoji}<br>
+        <p class="description">${item.description}</div>
+    </div>
+    `;
+    container.appendChild(newItem); 
+    newItem.addEventListener('click', function(){
+      newItem.classList.toggle("active");
+    });   
+  });
+}
+
+
+

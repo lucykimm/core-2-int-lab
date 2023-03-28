e7d19f5865c2be356b32732abb94ffe6a0b7bbfc
lucykimm <kimy528@newschool.edu>
2023-03-28 19:21:32 -0400

    0328-3

--- projects/project-2/js/script.js
+++ projects/project-2/js/script.js
@@ -11,46 +11,33 @@
     })
     .catch(error => console.error(error));
 }
-
-function displayData(data){
-
-  data.forEach( function(item, index){
+  function displayData(data){
+    data.forEach(function(item, index){
     console.log(item, index);
     let newItem = document.createElement("div");
-    newItem.innerHTML = 
- 
-      <div class="detail">     
-
-      <div class="Price">
-        <span class="label">Price</span> <br> ${item.Price}
+    newItem.innerHTML = `
+      <div class="Detail">
+        <div class="Price">
+          <span class="label">Price</span> <br> ${item.Price}
+        </div>
+        <div class="Brand">
+          <span class="label">Brand</span> <br> ${item.Brand}
+        </div>
+        <div class="Dimensions">
+          <span class="label">Dimensions</span> <br> ${item.Dimensions}
+        </div>
+        <div class="Designers">
+          <span class="label">Designers</span> <br> ${item.Designers}
+        </div>
+        <div class="Colors">
+          <span class="label">Colors</span> <br> ${item.Colors}
+        </div>
+        <div class="Materials">
+          <span class="label">Materials</span> <br> ${item.Materials}
+        </div>
       </div>
-
-      <div class="Brand">
-        <span class="label">Brand</span> <br> ${item.Brand}
-      </div>
-
-      <div class="Dimensions">
-        <span class="label">Dimensions</span> <br> ${item.Dimensions}
-      </div>
-
-      <div class="Designers">
-        <span class="label">Designers</span> <br> ${item.Designers}
-      </div>
-
-      <div class="Colors">
-        <span class="label">Colors</span> <br> ${item.Colors}
-      </div>
-
-      <div class="Materials">
-        <span class="label">Materials</span> <br> ${item.Materials}
-      </div>
-
-
-    </div>
-           
     `;
-
-
+    let container = document.querySelector(".container");
     container.appendChild(newItem); 
     newItem.addEventListener('click', function(){
       newItem.classList.toggle("active");

c1cc59815365546cb6e8be0e78ae0e97f95a2941
lucykimm <kimy528@newschool.edu>
2023-03-28 19:10:29 -0400

    0328-2

--- projects/project-2/js/script.js
+++ projects/project-2/js/script.js
@@ -1,16 +1,13 @@
   let container = document.getElementById('container');
 
-  const renderItems = (data) => {
+  function renderItems() {
     const lampList = document.getElementById("con");
-  //fetch JSON data
 
   fetch('./lamp.json')
   .then(response => response.json())
   .then(data => {
-//passes the data to the function above
     console.log(data)
     displayData(data);
-    //renders the items in reverse order
     })
     .catch(error => console.error(error));
 }
@@ -20,30 +17,47 @@ function displayData(data){
   data.forEach( function(item, index){
     console.log(item, index);
     let newItem = document.createElement("div");
-    newItem.innerHTML = `
-        <div class="img ${item.class}"><img src="img ${item.img}.png"></div>
-          <div class="Details">
-            <div class="img">
-              <img src="img/${item.img}.png">
-            </div>`;
+    newItem.innerHTML = 
+ 
+      <div class="detail">     
 
-    container.appendChild(newItem);
-    })
-  };
+      <div class="Price">
+        <span class="label">Price</span> <br> ${item.Price}
+      </div>
 
-  //         <div class="Info">          
-  //          <div class="Name">${item.Name}</div>
-  //           <div class="Price"><span class="label">Price</span> <br> ${item.Price}</div>
-  //           <div class="Brand"><span class="label">Brand</span> <br> ${item.Brand}</div>
-  //           <div class="Dimensions"><span class="label">Dimensions</span> <br> ${item.Dimensions}</div>
-  //           <div class="Designers"><span class="label">Designers</span> <br> ${item.Designers}</div>
-  //           <div class="Colors"><span class="label">Colors</span> <br> ${item.Colors}</div>
-  //          <div class="Materials"><span class="label">Materials</span> <br> ${item.Materials}</div>`;
-  //     container.appendChild(newItem);    
-      
-  //     newItem.addEventListener('click', function(){
-  //       console.log('click');
-  //       newItem.classList.toggle('active');
-  //     });
-  //   });
-  // }
+      <div class="Brand">
+        <span class="label">Brand</span> <br> ${item.Brand}
+      </div>
+
+      <div class="Dimensions">
+        <span class="label">Dimensions</span> <br> ${item.Dimensions}
+      </div>
+
+      <div class="Designers">
+        <span class="label">Designers</span> <br> ${item.Designers}
+      </div>
+
+      <div class="Colors">
+        <span class="label">Colors</span> <br> ${item.Colors}
+      </div>
+
+      <div class="Materials">
+        <span class="label">Materials</span> <br> ${item.Materials}
+      </div>
+
+
+    </div>
+           
+    `;
+
+
+    container.appendChild(newItem); 
+    newItem.addEventListener('click', function(){
+      newItem.classList.toggle("active");
+    });   
+  });
+}
+
+renderItems()
+
+  
